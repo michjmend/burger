f87a4af4716ee20d761a0d0ce45563c66d5b1c52
Peanut <michjmend@gmail.com>
2019-05-29 21:22:36 -0400

    html javascript

--- models/burger.js
+++ models/burger.js
@@ -2,24 +2,24 @@
 var orm = require("../config/orm.js");
 // Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
 var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+  insertOne: function(cols, vals, cb) {
+    orm.selectAll("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
+  updateOne: function(objColVals, condition, cb) {
+    orm.selectAll("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
+  deleteOne: function(condition, cb) {
+    orm.selectAll("burgers", condition, function(res) {
       cb(res);
     });
   }
--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// static files
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
