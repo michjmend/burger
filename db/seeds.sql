edc58553ada1d1d269140b3b1c18d28d35c89ac7
Peanut <michjmend@gmail.com>
2019-06-25 00:02:59 -0400

    css/html

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,5 +3,3 @@ INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Texas', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('California', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon and Avocado', true);
